1f2c628a3bd82e767f26270c4c6749de531c6339
Elina usenova <elina.u99@gmail.com>
2022-11-03 16:38:05 +0600

    TT-4 added user model

---
+++ backend/models/User.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const {nanoid} = require('nanoid');
+
+const Schema = mongoose.Schema;
+const SALT_WORK_FACTOR = 10;
+
+const validateUnique = async value => {
+    const user = await User.findOne({email: value});
+
+    if (user) return false;
+};
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: validateUnique,
+            message: 'This user is already registered',
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    token: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['cashier', 'admin'],
+    },
+});
+
+UserSchema.pre('save', async function(next) {
+    if (!this.isModified('password')) return next();
+
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    const hash = await bcrypt.hash(this.password, salt);
+
+    this.password = hash;
+
+    next();
+});
+
+UserSchema.set('toJSON', {
+    transform: (doc, ret) => {
+        delete ret.password;
+        return ret;
+    }
+});
+
+UserSchema.methods.checkPassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateToken = function() {
+    this.token = nanoid();
+};
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
