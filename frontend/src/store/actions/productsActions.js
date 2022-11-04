0d6d180440b0c1bc1b5386ccd280d1509ee9b374
Akbar Zakirov <zakirovakbar777@gmail.com>
2022-11-04 17:33:32 +0600

    TT-18 - Frontend: Added actions and slices for products(2)

--- frontend/src/store/actions/productsActions.js
+++ frontend/src/store/actions/productsActions.js
@@ -1,5 +1,14 @@
 import axiosApi from "../../axiosApi";
-import {fetchProductsFailure, fetchProductsRequest, fetchProductsSuccess} from "../slices/productsSlice";
+import {
+    createProductFailure,
+    createProductRequest, createProductSuccess, deleteProductFailure, deleteProductRequest, deleteProductSuccess,
+    fetchOneProductFailure,
+    fetchOneProductRequest, fetchOneProductSuccess,
+    fetchProductsFailure,
+    fetchProductsRequest,
+    fetchProductsSuccess
+} from "../slices/productsSlice";
+import {historyPush} from "./historyActions";
 
 
 export const fetchProducts = () => {
@@ -9,7 +18,47 @@ export const fetchProducts = () => {
             const response = await axiosApi('/products');
             dispatch(fetchProductsSuccess(response.data));
         } catch (e) {
-            dispatch(fetchProductsFailure(e.message));
+            dispatch(fetchProductsFailure(e.response.data));
+        }
+    }
+};
+
+export const fetchOneProduct = id => {
+    return async dispatch => {
+        try {
+            dispatch(fetchOneProductRequest());
+
+            const response = await axiosApi('/products/' + id);
+
+            dispatch(fetchOneProductSuccess(response.data));
+        } catch (e) {
+            dispatch(fetchOneProductFailure(e.response.data));
+        }
+    }
+};
+
+export const createProduct = (productData) => {
+    return async dispatch => {
+        try {
+            dispatch(createProductRequest());
+            await axiosApi.post('/products', productData);
+            dispatch(createProductSuccess());
+            dispatch(historyPush('/'));
+        } catch (e) {
+            dispatch(createProductFailure(e.response.data));
+        }
+    }
+};
+
+export const deleteProduct = id => {
+    return async dispatch => {
+        try{
+            dispatch(deleteProductRequest());
+            await axiosApi.delete('/products/' + id);
+            dispatch(deleteProductSuccess());
+            dispatch(historyPush('/'))
+        }catch (e) {
+            dispatch(deleteProductFailure(e.response.data));
         }
     }
 };
--- frontend/src/store/slices/productsSlice.js
+++ frontend/src/store/slices/productsSlice.js
@@ -6,6 +6,16 @@ export const initialState = {
     products: [],
     fetchLoading: false,
     fetchError: null,
+
+    product: null,
+    singleLoading: false,
+    singleError: null,
+
+    createProductLoading: false,
+    createProductError: null,
+
+    deleteLoading: false,
+    deleteError: null,
 };
 const productsSlice = createSlice({
     name,
@@ -23,12 +33,57 @@ const productsSlice = createSlice({
             state.fetchLoading = false;
             state.fetchError = error;
         },
+        createProductRequest(state) {
+            state.createProductLoading = true;
+            state.createProductError = null;
+        },
+        createProductSuccess(state) {
+            state.createProductLoading = false;
+            state.createProductError = null;
+        },
+        createProductFailure(state, {payload: error}) {
+            state.createProductLoading = false;
+            state.createProductError = error;
+        },
+        fetchOneProductRequest(state) {
+            state.singleLoading = true;
+            state.singleError = null;
+        },
+        fetchOneProductSuccess(state,{payload: product}) {
+            state.singleLoading = false;
+            state.product = product;
+        },
+        fetchOneProductFailure(state, {payload: error}) {
+            state.singleLoading = false;
+            state.singleError = error;
+        },
+        deleteProductRequest(state) {
+            state.deleteLoading = true;
+            state.deleteError = null;
+        },
+        deleteProductSuccess(state) {
+            state.deleteLoading = false;
+        },
+        deleteProductFailure(state, {payload: error}) {
+            state.deleteLoading = false;
+            state.deleteError = error;
+        },
+
     }
 });
 export const {
     fetchProductsRequest,
     fetchProductsSuccess,
     fetchProductsFailure,
+    fetchOneProductRequest,
+    fetchOneProductSuccess,
+    fetchOneProductFailure,
+    createProductRequest,
+    createProductSuccess,
+    createProductFailure,
+    deleteProductRequest,
+    deleteProductSuccess,
+    deleteProductFailure,
 } = productsSlice.actions
 
 export default productsSlice;
