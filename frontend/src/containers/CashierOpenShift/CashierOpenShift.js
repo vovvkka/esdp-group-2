6e136ef3a59c48892c6ff1fb2968c07cee80f0d2
Akbar Zakirov <zakirovakbar777@gmail.com>
2022-11-09 16:13:21 +0600

    TT-33 - Frontend: создать страницу для открытия смены - Updated-2.

--- frontend/src/containers/CashierOpenShift/CashierOpenShift.js
+++ frontend/src/containers/CashierOpenShift/CashierOpenShift.js
@@ -26,46 +26,49 @@ const CashierOpenShift = () => {
     const submitHandler = e => {
         e.preventDefault();
     };
-
-    return (
-        <Container maxWidth="xs">
-            <Grid sx={{backgroundColor: "#d4d4bc", borderRadius: "5px", textAlign: 'center'}}>
-                <div className={classes.paper}>
-                    <Grid  item sx={{marginY: "10px"}}>
-                        <Typography variant="h6"
-                                    sx={{backgroundColor: '#ff9994',
-                                        width: "350px",
-                                        height: '50px',
-                                        paddingTop: '10px'}}>
-                            СМЕНА ЗАКРЫТА
+        return (
+            <Container maxWidth="xs" sx={{marginTop: '150px'}}>
+                <Grid sx={{backgroundColor: "#d4d4bc", borderRadius: "5px", textAlign: 'center'}}>
+                    <div className={classes.paper}>
+                        <Grid item sx={{marginY: "10px"}}>
+                            <Typography variant="h6"
+                                        sx={{
+                                            backgroundColor: '#ff9994',
+                                            width: "350px",
+                                            height: '50px',
+                                            paddingTop: '10px'
+                                        }}>
+                                СМЕНА ЗАКРЫТА
                             </Typography>
-                    </Grid>
-            <Grid item sx={{marginY: "10px", backgroundColor: "#32a3ff", padding: "20px"}}>
-                <Typography component="h1" variant="h6">ПИН КОД</Typography>
-                    <TextField
-                        type="password"
-                        name="pinKod"
-                        value={state.pinKod}
-                        onChange={inputChangeHandler}
-                        bg="white"
-                        inputProps={{maxLength: 4}}
-                        sx={{backgroundColor: 'white'}}
-                    >
-                    </TextField>
-            </Grid>
-                    <Grid item sx={{marginY: "20px"}}>
-                        <ButtonWithProgress sx={{backgroundColor: '#4fbeff',
-                                    width: "200px"}}
-                                    variant="contained"
-                                    color="inherit"
-                                    onSubmit={submitHandler}>
-                            Открыть смену
-                        </ButtonWithProgress>
-                    </Grid>
-                </div>
-            </Grid>
-        </Container>
-    );
+                        </Grid>
+                        <Grid item sx={{marginY: "10px", backgroundColor: "#32a3ff", padding: "20px"}}>
+                            <Typography component="h1" variant="h6">ПИН КОД</Typography>
+                            <TextField
+                                type="password"
+                                name="pinKod"
+                                value={state.pinKod}
+                                onChange={inputChangeHandler}
+                                bg="white"
+                                inputProps={{maxLength: 4}}
+                                sx={{backgroundColor: 'white'}}
+                            >
+                            </TextField>
+                        </Grid>
+                        <Grid item sx={{marginY: "20px"}}>
+                            <ButtonWithProgress sx={{
+                                backgroundColor: '#4fbeff',
+                                width: "200px"
+                            }}
+                                                variant="contained"
+                                                color="inherit"
+                                                onSubmit={submitHandler}>
+                                Открыть смену
+                            </ButtonWithProgress>
+                        </Grid>
+                    </div>
+                </Grid>
+            </Container>
+        );
 };
 
 export default CashierOpenShift;
