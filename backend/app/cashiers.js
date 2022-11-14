399d493caf78e91f133de8a85a22b52a3d6120e9
Akbar Zakirov <zakirovakbar777@gmail.com>
2022-11-14 17:35:54 +0600

    TT-54 - Backend: создать роуты для кассиров - Updated

--- backend/app/cashiers.js
+++ backend/app/cashiers.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
     try {
-        const cashiers = await User.find({role: {$eq: 'cashiers'}});
+        const cashiers = await User.find({role: {$eq: 'cashier'}});
         const user = cashiers.findById(req.params.id);
         res.send(user);
     } catch (e) {
