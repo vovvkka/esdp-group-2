33b32c0035110e9813631ab1c7eb6b8de57dd719
Vladimir Baltiev <v.golem2288@gmail.com>
2022-11-16 16:41:27 +0600

    TT-63 added actions for edit cashier

--- backend/app/cashiers.js
+++ backend/app/cashiers.js
@@ -7,12 +7,45 @@ router.get('/', async (req, res) => {
         const cashiers = await User.find({role: {$eq: 'cashier'}});
 
         if (!cashiers) {
-            return res.status(404).send({message: 'Cashier not found!'});
+            return res.status(404).send({message: 'Кассир не найден.'});
         }
 
         res.send(cashiers);
     } catch (e) {
-        res.status(400).send(e);
+        res.status(500).send(e);
+    }
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const cashier = await User.findById(req.params.id);
+
+        if (!cashier) {
+            return res.status(404).send({message: 'Кассир не найден.'});
+        }
+
+        res.send(cashier);
+    } catch (e) {
+        res.status(500).send(e);
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const {username, password, displayName, pin, role} = req.body;
+    const userData = {username, password, displayName, pin, role};
+
+    try {
+        const cashier = await User.findById(req.params.id);
+
+        if (!cashier) {
+            return res.status(404).send({message: 'Кассир не найден.'});
+        }
+
+        await User.findByIdAndUpdate(req.params.id, userData);
+
+        res.send({message: 'Edit successful!'});
+    } catch (e) {
+        res.status(400).send({error: e.errors});
     }
 });
 
