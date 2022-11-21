c35d2f2083b52d3348cbf3341052347e269478ed
Tuibaev Jalyn <sharrder.blendi.1@gmail.com>
2022-11-21 15:40:15 +0600

    TT-76 - deleted extra files

--- frontend/src/containers/MainPage/MainPage.js
+++ frontend/src/containers/MainPage/MainPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import theme from "../../theme";
 import {Container, Stack, Typography, Box, useMediaQuery} from "@mui/material";
-import {UIProvider} from "../../context/ui";
 import Banner from "../../components/Banner/Banner";
 import {ThemeProvider} from "@mui/system";
 import Products from "../../components/Products/Products";
@@ -20,13 +19,11 @@ const MainPage = () => {
                 }}
             >
                 <Stack>
-                    <UIProvider>
-                        <Banner/>
-                        <Box display="flex" justifyContent="center" sx={{p: 4}}>
-                            <Typography variant="h4">Наши товары</Typography>
-                        </Box>
-                        <Products/>
-                    </UIProvider>
+                    <Banner/>
+                    <Box display="flex" justifyContent="center" sx={{p: 4}}>
+                        <Typography variant="h4">Наши товары</Typography>
+                    </Box>
+                    <Products/>
                 </Stack>
             </Container>
         </ThemeProvider>
