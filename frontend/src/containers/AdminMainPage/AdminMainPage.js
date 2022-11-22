43cf6ab4bab956eae0675acffd87dd101ad4ba35
Vladimir Baltiev <v.golem2288@gmail.com>
2022-11-22 16:53:58 +0600

    TT-89 added table on admin main page.

--- frontend/src/containers/AdminMainPage/AdminMainPage.js
+++ frontend/src/containers/AdminMainPage/AdminMainPage.js
@@ -1,13 +1,38 @@
-import React from 'react';
-import {Grid} from "@mui/material";
+import React, {useEffect} from 'react';
+import {Grid, Typography} from "@mui/material";
 import Catalog from "../../components/Catalog/Catalog";
 import {Box} from "@mui/system";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchShifts} from "../../store/actions/shiftsActions";
+import TableAdmin from "../../components/UI/Table/Table";
+import {makeStyles} from "tss-react/mui";
+
+const useStyles = makeStyles()(() => ({
+    boxScrolling: {
+        margin: '50px 100px 0 0',
+        height: '680px',
+        overflowY: 'scroll'
+    },
+}));
 
 const AdminMainPage = () => {
-    return (
-        <Grid container spacing={2}>
-            <Box>
+    const {classes} = useStyles();
+    const dispatch = useDispatch();
+    const shifts = useSelector(state => state.shifts.shifts);
 
+    useEffect(() => {
+        dispatch(fetchShifts());
+    }, [dispatch]);
+
+    const rowsHead = ['№', 'Кассир', 'Дата открытия смены', 'Дата закрытия смены'];
+
+    return (
+        <Grid container spacing={2} justifyContent="flex-end">
+            <Box className={classes.boxScrolling}>
+                {
+                    shifts?.length > 0 ? <TableAdmin rowsHead={rowsHead} rows={shifts} shifts='Смены'/>
+                        : <Typography variant='h6'>Смены не найдены</Typography>
+                }
             </Box>
             <Catalog/>
         </Grid>
