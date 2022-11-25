11c65a9b1cea8c9e654b8de38e10221ab7da2268
Elina usenova <elina.u99@gmail.com>
2022-11-25 21:11:49 +0600

    TT-102 added clearOrderError on return

--- frontend/src/components/OrderForm/OrderForm.js
+++ frontend/src/components/OrderForm/OrderForm.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import FormElement from "../UI/Form/FormElement/FormElement";
 import {Button, Grid} from "@mui/material";
-import {clearOrderError} from "../../store/slices/orderSlice";
 import {useDispatch} from "react-redux";
+import {clearOrderError} from "../../store/slices/ordersSlice";
 
 const OrderForm = ({onSubmit, error}) => {
     const dispatch = useDispatch();
