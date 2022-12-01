422cd7368be44e1396a9cf25b220759dbe80012d
lera <lera.24054@gmail.com>
2022-12-01 16:30:37 +0600

    TT-120 Fixed container

--- frontend/src/components/UI/Layout/Layout.js
+++ frontend/src/components/UI/Layout/Layout.js
@@ -4,17 +4,23 @@ import AppToolbar from "../AppToolBar/AppToolBar";
 import {ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from "../../NewShop/Footer/Footer";
+import {useLocation} from "react-router-dom";
 
 const Layout = ({children}) => {
+    const location = useLocation();
+
     return (
         <div className="wrapper">
             <ToastContainer toastStyle={{backgroundColor: "#dde6ff", color: '#000'}}/>
             <CssBaseline/>
             <AppToolbar/>
             <div className='main'>
-                <div className="container">
-                    {children}
-                </div>
+                {location.pathname.includes('cashier') || location.pathname.includes('admin') ?
+                        children :
+                    <div className="container">
+                        {children}
+                    </div>
+                }
             </div>
             <Footer/>
         </div>
