75eb3bcad8997b3b921d7fdaaf7f90d8d066f166
Vladimir Baltiev <v.golem2288@gmail.com>
2022-12-01 16:43:59 +0600

    TT-118 added page "Success Order Place", added adaptivity for page, changed some styles

--- frontend/src/containers/AddOrderProduct/AddOrderProduct.js
+++ frontend/src/containers/AddOrderProduct/AddOrderProduct.js
@@ -1,30 +1,33 @@
-import React from 'react';
+import React from "react";
 import OrderForm from "../../components/OrderForm/OrderForm";
-import {useDispatch, useSelector} from "react-redux";
-import {addOrder} from "../../store/actions/ordersActions";
+import { useDispatch, useSelector } from "react-redux";
+import { addOrder } from "../../store/actions/ordersActions";
 
 const AddOrderProduct = () => {
-    const errors = useSelector(state => state.orders.createError);
-    const products = useSelector(state => state.cart.products);
-    const dispatch = useDispatch();
+   const errors = useSelector((state) => state.orders.createError);
+   const products = useSelector((state) => state.cart.products);
+   const dispatch = useDispatch();
 
-    const addOrderData = async customerData => {
-        const order = products.map(product => ({product: product._id, quantity: product.quantity}));
-        const orderObj = {...customerData, order};
-        await dispatch(addOrder(orderObj));
-    };
-    return (
-        <div className='customer-order'>
-            <h2 className='customer-order__title'>
-                Оформление заказа
-            </h2>
-            <div className='customer-order__location'>Главная — <span className='customer-order__location-page'>Оформление заказа</span></div>
-            <OrderForm
-                error={errors}
-                onSubmit={addOrderData}
-            />
-        </div>
-    );
+   const addOrderData = async (customerData) => {
+      const order = products.map((product) => ({
+         product: product._id,
+         quantity: product.quantity,
+      }));
+      const orderObj = { ...customerData, order };
+      await dispatch(addOrder(orderObj));
+   };
+   return (
+      <div className="customer-order">
+         <h2 className="customer-order__title">Оформление заказа</h2>
+         <div className="customer-order__location">
+            Главная —{" "}
+            <span className="customer-order__location-page">
+               Оформление заказа
+            </span>
+         </div>
+         <OrderForm error={errors} onSubmit={addOrderData} />
+      </div>
+   );
 };
 
 export default AddOrderProduct;
