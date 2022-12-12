cb42b3c1bc97e1869e3758148c6acc847c28cb0e
Vladimir Baltiev <v.golem2288@gmail.com>
2022-12-12 19:56:34 +0600

    TT-145 added routers for get link for reset password, added route for reset password

--- backend/app/users.js
+++ backend/app/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const User = require("../models/User");
 const auth = require("../middlewares/auth");
 const permit = require("../middlewares/permit");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const nodemailer = require("nodemailer");
 const router = express.Router();
 
 router.get('/:id',auth,permit('admin'), async (req, res) => {
@@ -66,6 +69,75 @@ router.delete('/sessions', async (req, res) => {
     await user.save({validateBeforeSave: false});
 
     return res.send({success, user});
+});
+
+// reset-password //
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
+    }
+});
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+router.post('/forgot-password', async (req, res) => {
+   const { email } = req.body;
+
+   try {
+       const user = await User.findOne({email});
+
+       if (!user) {
+           return res.status(404).send({error: "Пользователь не найден."});
+       }
+
+       const secret = JWT_SECRET + user.password;
+       const payload = {
+           email: user.email,
+           id: user._id
+       };
+       const token = jwt.sign(payload, secret, {expiresIn: '15m'});
+
+       const link = `http://localhost:3000/reset-password/${user._id}/${token}`;
+
+       const mailOptions = {
+           from: "taitai.software@gmail.com",
+           to: email,
+           subject: "Reset Password",
+           html: `<h3>Тай Тай</h3> <p>Для того чтобы сбросить свой пароль, перейдите по ссылке: <br> ${link}</p> <p>Если это не вы, то просто проигнорируйте это письмо.</p>`
+       }
+
+       transporter.sendMail(mailOptions);
+       res.send('Ссылка для сброса пароля была отправлена на почту: ' + email);
+
+   } catch (e) {
+        res.status(401).send(e);
+   }
+});
+router.post('/reset-password/:id/:token', async (req, res) => {
+    const {id, token} = req.params;
+    const {password} = req.body;
+
+    try {
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(404).send({error: "Пользователь не найден."});
+        }
+
+        const secret = JWT_SECRET + user.password;
+
+        jwt.verify(token, secret);
+
+        user.password = password;
+        await user.save({validateBeforeSave: false});
+
+        res.send({message: "Пароль успешно изменен!"});
+    } catch (e) {
+        res.status(404).send(e);
+    }
 });
 
 module.exports = router;
