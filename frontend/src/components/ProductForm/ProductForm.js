7755da5b36ef5d58b68c5eb23cd6a755e42664ac
Elina usenova <elina.u99@gmail.com>
2022-12-17 01:30:03 +0600

    TT-160 installed package from AntDesign, used TreeSelect with async loading, modified code for product, category creating and editing

--- frontend/src/components/ProductForm/ProductForm.js
+++ frontend/src/components/ProductForm/ProductForm.js
@@ -3,6 +3,9 @@ import {Button, Grid} from "@mui/material";
 import FormElement from "../UI/Form/FormElement/FormElement";
 import FileInput from "../UI/Form/FileInput/FileInput";
 import FormSelect from "../UI/Form/FormSelect/FormSelect";
+import './ProductForm.css';
+import axiosApi from "../../axiosApi";
+import {TreeSelect} from "antd";
 
 const ProductForm = ({product, categories, error, onSubmit}) => {
     const [options, setOptions] = useState([]);
@@ -27,22 +30,72 @@ const ProductForm = ({product, categories, error, onSubmit}) => {
 
     useEffect(() => {
         if (product) {
-            setState({...product});
+            const newData = {...product};
+            newData.category = newData.category.title;
+
+            setState(newData);
         }
     }, [product]);
-    const submitFormHandler = e => {
+
+    const onSubmitHandler = e => {
         e.preventDefault();
-        const formData = new FormData();
-        Object.keys(state).forEach(key => {
-            if (key === 'image') {
-                state[key].forEach(item => {
-                    formData.append(`image`, item);
+        if (product){
+            console.log(0);
+            if(state.category===product.category.title){
+                const newData={...state};
+                newData.category=product.category._id;
+                const formData = new FormData();
+                Object.keys(newData).forEach(key => {
+                    if (key === 'image') {
+                        newData[key].forEach(item => {
+                            formData.append(`image`, item);
+                        });
+                    } else {
+                        formData.append(key, newData[key]);
+                    }
                 });
-            } else {
-                formData.append(key, state[key]);
+                onSubmit(formData);
+            }else {
+                console.log(1);
+
+                const formData = new FormData();
+                Object.keys(state).forEach(key => {
+                    if (key === 'image') {
+                        state[key].forEach(item => {
+                            formData.append(`image`, item);
+                        });
+                    } else {
+                        formData.append(key, state[key]);
+                    }
+                });
+                onSubmit(formData);
             }
+        }else {
+            console.log(3);
+
+            const formData = new FormData();
+            Object.keys(state).forEach(key => {
+                if (key === 'image') {
+                    state[key].forEach(item => {
+                        formData.append(`image`, item);
+                    });
+                } else {
+                    formData.append(key, state[key]);
+                }
+            });
+            onSubmit(formData);
+        }
+    };
+
+    const onLoadData = async ({id}) => {
+        const response = await axiosApi('/categories/?node=' + id);
+        setOptions(options.concat(response.data));
+
+    }
+    const onChange = (newValue) => {
+        setState(prevState => {
+            return {...prevState, category: newValue};
         });
-        onSubmit(formData);
     };
 
     const inputChangeHandler = e => {
@@ -71,6 +124,7 @@ const ProductForm = ({product, categories, error, onSubmit}) => {
 
     return (
         <form
+            onSubmit={onSubmitHandler}
             autoComplete="off"
         >
             <Grid
@@ -81,14 +135,27 @@ const ProductForm = ({product, categories, error, onSubmit}) => {
                 direction="column"
                 rowSpacing={2}
             >
-                <FormSelect
-                    onChange={inputChangeHandler}
-                    name='category'
-                    options={options}
-                    label='Категория'
+                <div style={{textAlign:'left'}}>
+                    <label>Категория</label>
+                    <div style={{color:'#dc4815'}}>{getFieldError('category')?'Заполните это поле':null}</div>
+
+                </div>
+                <TreeSelect
+                    treeDataSimpleMode
+                    style={{
+                        width: '100%',
+                        marginTop:'10px',
+                        marginBottom:'5px',
+                        textAlign:'left'
+                    }}
                     value={state.category}
-                    error={getFieldError('category')}
-                    selectFromServer
+                    dropdownStyle={{
+                        maxHeight: 400,
+                        overflow: 'auto',
+                    }}
+                    onChange={onChange}
+                    loadData={onLoadData}
+                    treeData={options}
                 />
 
                 <Grid display='flex' justifyContent='space-between' alignItems='center'>
@@ -199,8 +266,7 @@ const ProductForm = ({product, categories, error, onSubmit}) => {
 
                 <Grid item>
                     {}
-                    <Button type="submit" color="primary" variant="contained"
-                            onClick={submitFormHandler}>{product?'Редактировать':'Добавить'}</Button>
+                    <Button type="submit" color="primary" variant="contained">{product ? 'Редактировать' : 'Добавить'}</Button>
                 </Grid>
             </Grid>
         </form>
