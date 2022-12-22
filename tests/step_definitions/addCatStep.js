03a647d24b380bd8073e9c063398b9b289408611
Daniil Goncharenko <daniil.goncharenko.2020@gmail.com>
2022-12-22 17:42:42 +0600

    Tests-#TT-166 - refactor code

--- tests/step_definitions/addCatStep.js
+++ tests/step_definitions/addCatStep.js
@@ -27,12 +27,10 @@ Given('кликаю на первый селект и выбираю поле {s
 });
 
 Given('я введу данные', table => {
-
     table.rows.forEach(row => {
         const name = row.cells[0].value;
         const value = row.cells[1].value;
         I.fillField(name, value);
-
     });
 });
 
