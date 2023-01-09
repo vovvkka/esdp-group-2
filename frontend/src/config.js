313b52e26a35f1e420a04e374ebc5f949289c888
Tuibaev Jalyn <sharrder.blendi.1@gmail.com>
2023-01-09 22:28:17 +0600

    Edit config.js for deploy

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
-export let apiUrl = 'http://localhost:8000';
+export let apiUrl = 'http://188.226.163.6:8000';
 
 if (process.env.REACT_APP_ENV === 'test') {
-    apiUrl = 'http://localhost:8010'
+    apiUrl = 'http://188.226.163.6:8010'
 }
