2f768cf5e89f1b42106c19c8af61ec1e9315ca5b
Tuibaev Jalyn <sharrder.blendi.1@gmail.com>
2023-01-24 23:43:32 +0600

    Added production api endpoint

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,9 @@
 export let apiUrl = 'http://localhost:8000';
 
 if (process.env.REACT_APP_ENV === 'test') {
-    apiUrl = 'http://localhost:8010'
+    apiUrl = 'http://localhost:8010';
+}
+
+if (process.env.NODE_ENV === 'production') {
+    apiUrl = 'http://188.226.163.6:8000';
 }
