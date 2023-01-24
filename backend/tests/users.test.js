e861853422fda18f55a41bb5bea5a3464ba3a9d4
Daniil Goncharenko <daniil.goncharenko.2020@gmail.com>
2023-01-24 17:21:49 +0600

    Tests-#TT-199 write a unit test for login

--- backend/tests/users.test.js
+++ backend/tests/users.test.js
@@ -1,47 +1,39 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app  = require("../index");
-const {nanoid} = require("nanoid");
 
 describe('Testing \'users\' route', () => {
     let admin;
-    let cashier;
-    const newAdmin = {
-        username: 'admin',
-        displayName: 'Заказчик Тай-Тай',
-        password: 'admin0',
-        email: 'v.golem228@gmail.com',
-        pin: 1234,
-        token: nanoid(),
-        role: 'admin',
-    };
-    const newCashier = {
-        username: 'cashier 2',
-        displayName: 'Сычев Виталий',
-        password: 'cashier2',
-        email: 'vitalya@gmail.com',
-        pin: 2222,
-        token: nanoid(),
-        role: 'cashier',
-    };
-    describe('test of user login', () => {
+
+    describe('POST /users/sessions', () => {
         it('user should successfully login', async () => {
             const res = await request(app)
-                .post('/users')
-                .send(newAdmin);
-            expect(res.statusCode).toBe(401);
+                .post('/users/sessions')
+                .send({
+                    username: 'admin',
+                    password: 'admin0',
+                });
+            expect(res.statusCode).toBe(200);
             admin = res.body;
         });
     });
-    describe('save new user', () => {
-        it('should save new user on db', async () => {
+
+    describe('POST /users', () => {
+        it('creation cashiers', async () => {
             const res = await request(app)
                 .post('/users')
-                .send(newCashier);
-            expect(res.statusCode).toBe(401);
-            cashier = res.body;
+                .send({
+                    username: 'casie2sdsrs2',
+                    password: 'cashirsdf2s222',
+                    displayName: 'c2assdfrs2000',
+                    pin: '1434',
+                    role: 'cashier',
+                    email: 'e2i2222sdf23l@gmail.com',
+                })
+                .set({Authorization: admin.user.token });
+            expect(res.statusCode).toBe(200);
         });
     });
 });
-afterAll(() => mongoose.disconnect());
 
+afterAll(() => mongoose.disconnect());
